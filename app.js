b9aa18521e18cdb17611a398edfe06a2b68f80c3
takamine_asuki <takamine_asuki@cyberagent.co.jp>
2013-08-04 15:43:06 +0900

    ログイン・ログアウト

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
-app.use(express.cookieParser('your secret here'));
+app.use(express.cookieParser('takamineasukikey'));
 app.use(express.session());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
@@ -30,7 +30,10 @@ if ('development' == app.get('env')) {
 }
 
 app.get('/', routes.index);
+app.post('/', routes.login, routes.index);
+app.del('/', routes.logout, routes.index);
 app.get('/users', user.list);
+app.get('/:name', routes.name);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
