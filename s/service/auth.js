3df5f50380cdde3dc72a5eebce3ac0caba5c595c
AoiAsu <ds107192@gmail.com>
2013-08-07 22:32:37 +0900

    create login lockout

--- s/service/auth.js
+++ s/service/auth.js
@@ -1,7 +1,9 @@
 
 var async = require('async');
 
-var mongo = mongo.get('User');
+var mongo = require('../mongo');
+
+var userMongo = mongo.get('User');
 
 function AuthService() {
 
@@ -10,18 +12,82 @@ function AuthService() {
 module.exports = new AuthService();
 
 AuthService.prototype.login = function(id, pwd, callback) {
+    var user;
+    var now = Date.now();
     async.series([
-        function() {
+        function(next) {
             // ユーザー情報取得
+            userMongo.get({ _id: id }, {}, function(err, result) {
+                if (err) {
+                    console.log('failed get User. id:', userId);
+                    return next(err);
+                }
+                if (!result) {
+                    return next({name: '会員ではない'});
+                }
+                user = result;
+                return next();
+            });
         },
-        function() {
-            // ロックアウト対象か確認
+        function(next) {
+            // 前回失敗していない
+            if (!user.login || !user.login.failure) {
+                return next();
+            }
+            var failure = user.login.failure;
+            // 前回の失敗から一定時間経っている
+            if (failure.time - now > 60 * 1000) {
+                return next();
+            }
+
+            // 連続指定回以上失敗している
+            if (failure.count >= 5) {
+                return next();
+            }
+
+            return next({name: 'ロックアウト', msg: 'アカウントロック中です。しばらく待ってから試してください。'});
+        },
+        function(next) {
             // パスワードが一致するか確認
-        },
-        function() {
-            // 一致したら、ログイン日時書き込み
+            if (pwd !== user.pwd) {
+                return next({name: 'パスワードが不一致'});
+            }
+
+            // ログイン日時書き込み
+            var data = {
+                $set: {
+                    success: {
+                        time: now
+                    }
+                },
+                $unset: { failure: true }
+            };
+            userMongo.update({ _id: id }, data, function(err) {
+                if (err) {
+                    console.log('failed login success update. id:', id);
+                    return next(err);
+                }
+                return next();
+            });
         }
     ], function(err) {
-        // パスワードが一致しなかったときは、failureに日時とカウント書き込み
+        var failedError = { name: 'ログイン失敗', msg: 'ログインに失敗しました。' };
+        if (err.name === 'ロックアウト') {
+            return callback(err);
+        }
+        if (err.name !== 'パスワードが不一致') {
+            return callback(failedError);
+        }
+        // failureに日時とカウント書き込み
+        var data = {
+            $set: { failure: { time: now }},
+            $inc: 1
+        };
+        userMongo.update( { _id: id }, data, function(err) {
+            if (err) {
+                return callback(failedError);
+            }
+            return callback();
+        });
     });
 };
